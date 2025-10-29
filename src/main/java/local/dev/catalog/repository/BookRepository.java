9e26cf187dc44668d354cdda62a742c26f1dc08c
UrbanDave <daevu2k69@gmail.com>
2025-10-29 22:44:55 +0100

    BookRepository mit Suchmethode implementiert AB#65

---
+++ src/main/java/local/dev/catalog/repository/BookRepository.java
@@ -0,0 +1,23 @@
+package local.dev.catalog.repository;
+
+import local.dev.catalog.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book,String> {
+
+    @Query("SELECT DISTINCT b FROM Book b WHERE " +
+            "LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(b.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(b.author) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Book> searchBooks(@Param("keyword") String keyword);
+
+    default List<Book> searchBooksByKeywords(String searchTerm) {
+        return searchBooks(searchTerm);
+    }
+
+}
