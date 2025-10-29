4d90f0243735c2e284a95c5fbd6ea97dd6e939e9
UrbanDave <daevu2k69@gmail.com>
2025-10-29 20:07:28 +0100

    Spring Boot projekt initialisiert AB#63

---
+++ src/test/java/local/dev/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package local.dev.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
