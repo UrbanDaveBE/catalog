437808995c4bb106087a245855c43fc32b7b0777
UrbanDave <daevu2k69@gmail.com>
2026-01-14 02:28:39 +0100

    feat: QoL improvement. added volume for postgres db, and initially loaded init_postgres_import.sql - books are now always stored in postgesdb and no load needed.

---
+++ src/main/resources/init_postgres_import.sql
@@ -0,0 +1,9 @@
+INSERT INTO book (isbn, title, description, author) VALUES
+                                                        ('978-0-7475-3269-9', 'Harry Potter and the Philosopher''s Stone', 'The boy who lived enters Hogwarts.', 'J.K. Rowling'),
+                                                        ('978-0-7475-3849-3', 'Harry Potter and the Chamber of Secrets', 'The Chamber of Secrets has been opened.', 'J.K. Rowling'),
+                                                        ('978-0-7475-4215-5', 'Harry Potter and the Prisoner of Azkaban', 'Sirius Black has escaped.', 'J.K. Rowling'),
+                                                        ('978-0-618-00221-3', 'The Hobbit', 'In a hole in the ground there lived a hobbit.', 'J.R.R. Tolkien'),
+                                                        ('978-0-618-00222-0', 'The Fellowship of the Ring', 'One Ring to rule them all.', 'J.R.R. Tolkien'),
+                                                        ('978-0-345-39180-3', 'The Hitchhiker''s Guide to the Galaxy', 'Don''t Panic.', 'Douglas Adams'),
+                                                        ('978-0-451-52493-5', '1984', 'Big Brother is watching you.', 'George Orwell'),
+                                                        ('978-0-06-112008-4', 'To Kill a Mockingbird', 'Shoot all the bluejays you want, if you can hit ''em.', 'Harper Lee');
